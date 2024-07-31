8ca138a45167180479a0cbb37b997fc47cc389d2
marcustar <859783525@qq.com>
2024-08-01 01:30:01 +0800

    fix index broken link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="/blog">
             site is being Building
           </Link>
         </div>
